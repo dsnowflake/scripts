8e9bfc4b9f5e6277a8bff437fcebada5707454af
dsnowflake <sadrul.email3@gmail.com>
2025-02-18 12:12:58 +0600

    Update and rename V2.1.1__deploy_dept.sql to V3.1.1__deploy_dept.sql

--- migrations/V3.1.1__deploy_dept.sql
+++ migrations/V3.1.1__deploy_dept.sql
@@ -1,9 +1,9 @@
-CREATE OR REPLACE TABLE dept (
+CREATE OR REPLACE TABLE dept1 (
     dept_id INT,          -- Department ID (integer)
     dept_name STRING      -- Department Name (string)
 );
 
-INSERT INTO dept (dept_id, dept_name) VALUES
+INSERT INTO dept1 (dept_id, dept_name) VALUES
     (1, 'Sales'),
     (2, 'Marketing'),
     (3, 'Engineering'),

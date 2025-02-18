e55acc7cbf8a5740d43b5508ce464889c9b412a1
dsnowflake <sadrul.email3@gmail.com>
2025-02-18 11:47:19 +0600

    Update V2.1.1__deploy_dept.sql

--- migrations/V2.1.1__deploy_dept.sql
+++ migrations/V2.1.1__deploy_dept.sql
@@ -2,6 +2,7 @@ CREATE OR REPLACE TABLE dept (
     dept_id INT,          -- Department ID (integer)
     dept_name STRING      -- Department Name (string)
 );
+
 INSERT INTO dept (dept_id, dept_name) VALUES
     (1, 'Sales'),
     (2, 'Marketing'),
